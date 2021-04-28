a2cf59183538a0d8c7daa7e76d54791d9b97149c
werpie <83338547+werpie@users.noreply.github.com>
2021-04-28 10:41:41 +0200

    dodałam plik
    z łańcuchem i pętlą

---
+++ bez nazwy.py
@@ -0,0 +1,17 @@
+lancuch = "gnaowen"
+print(lancuch)
+
+lancuch_lista = list(lancuch)
+nowy = []
+for i in range(3):
+    znak1=input("podaj jaki znak zamienic")
+    znak2=input("na jaki go zamienic?")
+    for litera in lancuch_lista:
+        if litera==znak1:
+            nowy+=znak2
+        else:
+            nowy+=litera
+    nowy = "".join(nowy)
+    print(nowy)
+    lancuch_lista=nowy
+    nowy=[]
